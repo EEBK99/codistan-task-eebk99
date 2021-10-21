b41adf6f361dbf54dff5ae65cef0083ef8ee1fe3
EEBK99 <haseeb_f_khan@outlook.com>
2021-10-21 16:24:48 +0500

    files added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import "./App.css";
-import { version } from "antd";
+import { version, Layout } from "antd";
 import styled from "styled-components";
 
 function App() {
-  const Button = styled.button`
-    color: palevioletred;
-    font-size: 1em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid palevioletred;
-    border-radius: 3px;
-  `;
+  var { Header, Footer, Sider, Content } = Layout;
 
-  const TomatoButton = styled(Button)`
-    color: tomato;
-    border-color: tomato;
+  // const Button = styled.button`
+  //   color: palevioletred;
+  //   font-size: 1em;
+  //   margin: 1em;
+  //   padding: 0.25em 1em;
+  //   border: 2px solid palevioletred;
+  //   border-radius: 3px;
+  // `;
+
+  // const TomatoButton = styled(Button)`
+  //   color: tomato;
+  //   border-color: tomato;
+  // `;
+
+  Header = styled(Header)`
+    background-color: white;
   `;
 
   return (
     <div className="App">
-      <h1>{version}</h1>
+      {/* <h1>{version}</h1>
       <Button>Normal Button</Button>
-      <TomatoButton>Tomato Button</TomatoButton>
+      <TomatoButton>Tomato Button</TomatoButton> */}
+
+      <Layout>
+        <Header>Header</Header>
+        <Layout>
+          <Sider>Sider</Sider>
+          <Content>Content</Content>
+        </Layout>
+        {/* <Footer>Footer</Footer> */}
+      </Layout>
     </div>
   );
 }
