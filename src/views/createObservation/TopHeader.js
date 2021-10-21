d073dbdb3ea4b81f8d0e109fbb672c12ec1a2533
EEBK99 <haseeb_f_khan@outlook.com>
2021-10-21 21:13:27 +0500

    updated

--- src/views/createObservation/TopHeader.js
+++ src/views/createObservation/TopHeader.js
@@ -3,13 +3,44 @@ import styled from "styled-components";
 import TopHeaderLogo from "../../assets/svg/TopHeaderLogo.svg";
 
 const TopHeader = () => {
-  const Nav = styled.div``;
+  const Nav = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  `;
+
+  const Logo = styled.a``;
+
+  const Humburger = styled.div``;
+
+  const Menu = styled.div``;
+
+  const MenuItems = styled.div`
+    display: inline-flex;
+  `;
 
   return (
     <Nav>
-      <a href="#">
-        <img src={TopHeaderLogo} alt="Safety Connect logo" />
-      </a>
+      <Logo href="#">
+        <img
+          //   style={{ width: 218.3, height: 41.45 }}
+          src={TopHeaderLogo}
+          alt="Safety Connect logo"
+        />
+      </Logo>
+
+      <Humburger class="TopHeader__Hamburger">
+        <span />
+        <span />
+        <span />
+      </Humburger>
+
+      <Menu>
+        <MenuItems>Search</MenuItems>
+        <MenuItems>Setting</MenuItems>
+        <MenuItems>Notification</MenuItems>
+        <MenuItems>Profile icon</MenuItems>
+      </Menu>
     </Nav>
   );
 };
