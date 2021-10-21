2c6e15b71f1a7b233c75e5eb4db4a6c6ed2f352b
EEBK99 <haseeb_f_khan@outlook.com>
2021-10-21 23:40:23 +0500

    Done Align and Responsive

--- src/views/createObservation/TopHeader.js
+++ src/views/createObservation/TopHeader.js
@@ -1,51 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import TopHeaderLogo from "../../assets/svg/TopHeaderLogo.svg";
 
 const TopHeader = () => {
-  const Nav = styled.div`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-  `;
-
-  const Logo = styled.a``;
-
-  const Humburger = styled.div`
-    display: none;
-    justify-content: center;
-    flex-direction: column;
-
-    span {
-      height: 2px;
-      width: 22px;
-      background: white;
-      margin-bottom: 4px;
-      border-radius: 5px;
-    }
-
-    @media (max-width: 768px) {
-      display: flex;
-    }
-  `;
-
-  const Menu = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: flex-start;
-    position: relative;
-
-    @media (max-width: 768px) {
-      overflow: hidden;
-      flex-direction: column;
-      width: 100%;
-    }
-  `;
-
-  const MenuItems = styled.div`
-    padding: 0rem 0.225rem;
-    text-align: start;
-  `;
+  const [isOpen, setisOpen] = useState(false);
 
   return (
     <Nav>
@@ -57,13 +15,16 @@ const TopHeader = () => {
         />
       </Logo>
 
-      <Humburger class="TopHeader__Hamburger">
+      <Humburger
+        class="TopHeader__Hamburger"
+        onClick={() => setisOpen(!isOpen)}
+      >
         <span />
         <span />
         <span />
       </Humburger>
 
-      <Menu>
+      <Menu isOpen={isOpen}>
         <MenuItems>Search</MenuItems>
         <MenuItems>Setting</MenuItems>
         <MenuItems>Notification</MenuItems>
@@ -72,5 +33,54 @@ const TopHeader = () => {
     </Nav>
   );
 };
+
+const Nav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Logo = styled.a``;
+
+const Humburger = styled.div`
+  display: none;
+  justify-content: center;
+  flex-direction: column;
+  cursor: pointer;
+
+  span {
+    height: 2px;
+    width: 22px;
+    background: white;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: relative;
+
+  @media (max-width: 768px) {
+    overflow: hidden;
+    flex-direction: column;
+    width: 100%;
+    background-color: #112565;
+    /* here it is checking eitheir it has to open or not using isOpen props  */
+    max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
+    transition: max-height 0.3s ease-in;
+  }
+`;
+
+const MenuItems = styled.div`
+  padding: 0rem 0.225rem;
+  text-align: start;
+`;
 
 export default TopHeader;
