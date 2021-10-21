e5d109b5563e852dd9ec482a6dfa512c32a00518
EEBK99 <haseeb_f_khan@outlook.com>
2021-10-22 01:51:42 +0500

    Done Design, Align and Responsive

--- src/views/createObservation/TopHeader.js
+++ src/views/createObservation/TopHeader.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import TopHeaderLogo from "../../assets/svg/TopHeaderLogo.svg";
+import TopHeaderSettingIcon from "../../assets/svg/TopHeaderSettingIcon.svg";
+import TopHeaderNotificationIcon from "../../assets/svg/TopHeaderNotifiIcon.svg";
+import { Button, Tooltip, Avatar, Badge } from "antd";
+import { SearchOutlined, UserOutlined } from "@ant-design/icons";
 
 const TopHeader = () => {
   const [isOpen, setisOpen] = useState(false);
@@ -8,13 +12,8 @@ const TopHeader = () => {
   return (
     <Nav>
       <Logo href="#">
-        <img
-          //   style={{ width: 218.3, height: 41.45 }}
-          src={TopHeaderLogo}
-          alt="Safety Connect logo"
-        />
+        <img src={TopHeaderLogo} alt="Safety Connect logo" />
       </Logo>
-
       <Humburger
         class="TopHeader__Hamburger"
         onClick={() => setisOpen(!isOpen)}
@@ -25,10 +24,33 @@ const TopHeader = () => {
       </Humburger>
 
       <Menu isOpen={isOpen}>
-        <MenuItems>Search</MenuItems>
-        <MenuItems>Setting</MenuItems>
-        <MenuItems>Notification</MenuItems>
-        <MenuItems>Profile icon</MenuItems>
+        <MenuItems>
+          <Button className="MenuItems__Search">
+            Search <span style={{ width: "150px" }}> </span> <SearchOutlined />
+          </Button>
+        </MenuItems>
+
+        <MenuItems>
+          <Tooltip title="Settings">
+            <img src={TopHeaderSettingIcon} alt="Setting Icon" />
+          </Tooltip>
+        </MenuItems>
+
+        <MenuItems>
+          <Badge style={{ background: "red" }} size="small" count={2}>
+            <Tooltip title="Notifications">
+              <img src={TopHeaderNotificationIcon} alt="Notification Icon" />
+            </Tooltip>
+          </Badge>
+        </MenuItems>
+
+        <MenuItems>
+          <Badge style={{ background: "red" }} size="small" count={1}>
+            <Tooltip title="Profile">
+              <Avatar icon={<UserOutlined />} />
+            </Tooltip>
+          </Badge>
+        </MenuItems>
       </Menu>
     </Nav>
   );
@@ -63,24 +85,27 @@ const Humburger = styled.div`
 
 const Menu = styled.div`
   display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
+  justify-content: flex-end;
+  align-items: flex-end;
   position: relative;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 
   @media (max-width: 768px) {
     overflow: hidden;
-    flex-direction: column;
+    flex-direction: row;
     width: 100%;
     background-color: #112565;
     /* here it is checking eitheir it has to open or not using isOpen props  */
     max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
-    transition: max-height 0.3s ease-in;
+    transition: max-height 0.6s ease-in;
   }
 `;
 
 const MenuItems = styled.div`
-  padding: 0rem 0.225rem;
+  padding: 0rem 0.525rem;
   text-align: start;
+  cursor: pointer;
 `;
 
 export default TopHeader;
