74e671635b68241d02ed1e2b8e5816cdbe6de3a4
EEBK99 <haseeb_f_khan@outlook.com>
2021-10-24 17:54:41 +0500

    updated

--- src/views/layout/LayoutStructure.js
+++ src/views/layout/LayoutStructure.js
@@ -4,6 +4,14 @@ import styled from "styled-components";
 import TopHeader from "../container/TopHeader";
 import MainContent from "../createObservation/MainContent";
 import LeftNavSiderHomeSvg from "../../assets/svg/LeftNavSiderHome.svg";
+import LeftNavSiderMyResSvg from "../../assets/svg/LeftNavSiderMyRes.svg";
+import LeftNavSiderObsFedSvg from "../../assets/svg/LeftNavSiderObsFed.svg";
+import LeftNavSiderIncManSvg from "../../assets/svg/LeftNavSiderIncMan.svg";
+import LeftNavSiderAudInsSvg from "../../assets/svg/LeftNavSiderAudIns.svg";
+import LeftNavSiderRiskMangSvg from "../../assets/svg/LeftNavSiderRiskMang.svg";
+import LeftNavSiderLearMangSvg from "../../assets/svg/LeftNavSiderLearMang.svg";
+import LeftNavSiderPerAnaSvg from "../../assets/svg/LeftNavSiderPerAna.svg";
+import LeftNavSiderYourProjSvg from "../../assets/svg/LeftNavSiderYourProj.svg";
 
 const LayoutStructure = () => {
   return (
@@ -13,21 +21,96 @@ const LayoutStructure = () => {
           <TopHeader />
         </Header>
         <Layout>
-          <Sider breakpoint="lg" collapsedWidth="0" theme="light">
-            <Menu theme="light" mode="inline" defaultSelectedKeys={["1"]}>
+          <Sider breakpoint="lg" collapsedWidth="0" theme="light" width="240">
+            <Menu theme="light" mode="inline" defaultSelectedKeys={["3"]}>
               <Menu.Item
                 key="1"
                 icon={<img src={LeftNavSiderHomeSvg} alt="" />}
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  color: "#969696",
+                }}
               >
                 Home
               </Menu.Item>
-              <Menu.Item key="2"> My Responsibilities </Menu.Item>
-              <Menu.Item key="3"> Observation & Feedback </Menu.Item>
-              <Menu.Item key="4"> Incident Management </Menu.Item>
-              <Menu.Item key="5"> Audit and Inspection </Menu.Item>
-              <Menu.Item key="6"> Risk Management </Menu.Item>
-              <Menu.Item key="7"> Learning Management </Menu.Item>
-              <Menu.Item key="8"> Performance Analytics </Menu.Item>
+              <Menu.Item
+                key="2"
+                icon={<img src={LeftNavSiderMyResSvg} alt="" />}
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  color: "#969696",
+                }}
+              >
+                My Responsibilities
+              </Menu.Item>
+              <Menu.Item
+                key="3"
+                icon={<img src={LeftNavSiderObsFedSvg} alt="" />}
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  color: "#969696",
+                }}
+              >
+                Observation & Feedback
+              </Menu.Item>
+              <Menu.Item
+                key="4"
+                icon={<img src={LeftNavSiderIncManSvg} alt="" />}
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  color: "#969696",
+                }}
+              >
+                Incident Management
+              </Menu.Item>
+              <Menu.Item
+                key="5"
+                icon={<img src={LeftNavSiderAudInsSvg} alt="" />}
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  color: "#969696",
+                }}
+              >
+                Audit and Inspection
+              </Menu.Item>
+              <Menu.Item
+                key="6"
+                icon={<img src={LeftNavSiderRiskMangSvg} alt="" />}
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  color: "#969696",
+                }}
+              >
+                Risk Management
+              </Menu.Item>
+              <Menu.Item
+                key="7"
+                icon={<img src={LeftNavSiderLearMangSvg} alt="" />}
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  color: "#969696",
+                }}
+              >
+                Learning Management
+              </Menu.Item>
+              <Menu.Item
+                key="8"
+                icon={<img src={LeftNavSiderPerAnaSvg} alt="" />}
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  color: "#969696",
+                }}
+              >
+                Performance Analytics
+              </Menu.Item>
             </Menu>
           </Sider>
 
